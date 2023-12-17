831a79cfbe038eaab7c969d81f94a0969e30a4a5
rails_dev <ishii.yuuki.infratop@gmail.com>
2023-12-17 15:02:57 +0900

    TC2-1 updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
