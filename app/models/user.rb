5f3474f7d1135033e53ccc541cf93f18bbea1b7f
GotouAsahi <gotou.asahi.infratop@gmail.com>
2023-12-17 14:36:03 +0900

    Interaction-2 finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,11 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-
   def get_profile_image(weight = 100, height = 100)
-    unless self.profile_image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    self.profile_image.variant(resize_to_fill: [weight,height]).processed
+    profile_image.variant(resize_to_fill: [weight, height]).processed
   end
 end
