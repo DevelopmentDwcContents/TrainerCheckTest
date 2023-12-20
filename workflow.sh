882ea32812525b08bae6a8f258cc3bed5653b8b8
rails_dev <ishii.yuuki.infratop@gmail.com>
2023-12-20 20:21:27 +0900

    test

--- workflow.sh
+++ workflow.sh
@@ -12,11 +12,10 @@ for git_branch in $branch_arr; do
     echo "branch = $git_branch"
     git switch $git_branch
     git branch
-    # touch question_description.md
-    # git stash apply $1
-    # git add .
-    # git commit -m "update"
-    # git push origin $git_branch
+    git stash apply $1
+    git add .
+    git commit -m "update"
+    git push origin $git_branch
 done
 
 git switch main
