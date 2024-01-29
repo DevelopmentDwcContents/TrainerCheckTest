a3fc2c0b8389a0589805561e1461fa992b095169
rails_dev <ishii.yuuki.infratop@gmail.com>
2024-01-29 23:15:06 +0900

    update

---
+++ sheets/synthesis-1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# delete unstaged changes
+git checkout -- .
+
+# move to main branch
+git switch main
+
+# resetup application
+rails db:migrate:reset
+rails db:seed
+
+# move to test branch
+git switch TrainerCheck2-1
